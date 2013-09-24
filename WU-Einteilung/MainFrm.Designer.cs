739fb9be33517eaf67577fae3f8a1244b6d70434
Just1Number <johannes@wolfs.de>
2013-09-24 14:39:18 +0200

    Das Dokument wird ausgelesen und alle wichtigen informationen in arrays geschreiben

--- WU-Einteilung/MainFrm.Designer.cs
+++ WU-Einteilung/MainFrm.Designer.cs
@@ -28,21 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbx_path = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbx_path
+            // 
+            this.tbx_path.Location = new System.Drawing.Point(346, 26);
+            this.tbx_path.Name = "tbx_path";
+            this.tbx_path.Size = new System.Drawing.Size(238, 20);
+            this.tbx_path.TabIndex = 1;
+            this.tbx_path.Text = "D:\\Users\\Johannes\\Desktop\\test";
+            this.tbx_path.TextChanged += new System.EventHandler(this.tbx_path_TextChanged);
+            // 
+            // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(852, 476);
+            this.Controls.Add(this.tbx_path);
+            this.Controls.Add(this.button1);
+            this.Name = "MainFrm";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.MainFrm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbx_path;
     }
 }
 
--- WU-Einteilung/MainFrm.cs
+++ WU-Einteilung/MainFrm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,75 @@ namespace WU_Einteilung
 {
     public partial class MainFrm : Form
     {
+        private Worksheet slist;
+        private Worksheet klist;
+        private Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+        private Microsoft.Office.Interop.Excel.Workbook wb;
+        private string document_path;
+        private int[] schueler_id;
+        private string[] schueler_namen;
+        private string[] schueler_vornamen;
+        private string[] schueler_klasse;
+        private string[] schueler_klassenlehrer;
+        private string[] schueler_erstwahl;
+        private string[] schueler_zweitwahl;
+        private string[] schueler_drittwahl;
+        private string[] kurse_id;
+        private int[] kurse_maxpersonen;
+        private int[] kurse_minpersonen;
+        private bool[] kurse_klasse8;
+        private bool[] kurse_klasse9;
+
         public MainFrm()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void Read_Document(string path)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            document_path = tbx_path.Text;
+            wb = app.Workbooks.Open(@document_path);
+            slist = (Worksheet)wb.Sheets[1];
+            klist = (Worksheet)wb.Sheets[2];
+            Range slist_range = slist.UsedRange;
+            Range klist_range = klist.UsedRange;
+            for (int i = 0; String.Equals(slist_range.Cells[i+2, 2].Value,""); i++)
+            {
+                slist_range.Cells[i+2, 1].Value = i;
+                schueler_id[i]           = slist_range.Cells[i+2, 1].Value;
+                schueler_namen[i]        = slist_range.Cells[i+2, 2].Value;
+                schueler_vornamen[i]     = slist_range.Cells[i+2, 3].Value;
+                schueler_klasse[i]       = slist_range.Cells[i+2, 4].Value;
+                schueler_klassenlehrer[i]= slist_range.Cells[i+2, 5].Value;
+                schueler_erstwahl[i]     = slist_range.Cells[i+2, 6].Value;
+                schueler_zweitwahl[i]    = slist_range.Cells[i+2, 7].Value;
+                schueler_drittwahl[i]    = slist_range.Cells[i+2, 8].Value;
+            }
+            for (int i = 0; String.Equals(klist_range.Cells[i+2, 1].Value,""); i++)
+            {
+                kurse_id[i] = klist_range.Cells[i + 2, 1].value;
+                kurse_maxpersonen[i] = klist_range.Cells[i + 2, 8].value;
+                kurse_minpersonen[i] = klist_range.Cells[i + 2, 7].value;
+                if (klist_range.Cells[i + 2, 4].Value == 1) kurse_klasse8[i] = true; else kurse_klasse8[i] = false;
+                if (klist_range.Cells[i + 2, 5].Value == 1) kurse_klasse9[i] = true; else kurse_klasse9[i] = false;
+            }
+            //a1.Value = "1";
+            //string cellValue=a1.Value;
+            //MessageBox.Show(cellValue);
+            wb.Save();
+            wb.Close();
+        }
+
+        private void tbx_path_TextChanged(object sender, EventArgs e)
         {
 
         }
