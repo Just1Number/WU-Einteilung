d180658a90ca8609a0139f552851ba40a0d1972f
Just1Number <johannes@wolfs.de>
2013-09-26 16:37:00 +0200

    added more stuff

--- WU-Einteilung/MainFrm.cs
+++ WU-Einteilung/MainFrm.cs
@@ -53,7 +53,7 @@ namespace WU_Einteilung
             klist = (Worksheet)wb.Sheets[2];
             slist_range = slist.UsedRange;
             klist_range = klist.UsedRange;
-            for (int i = 0; !String.Equals(slist_range.Cells[i+2, 2].Value,""); i++)
+            for (int i = 0; !String.Equals(slist_range.Cells[i+3, 2].Value,""); i++)
             {
                 slist_range.Cells[i+2, 1].Value = i;
                 schueler_id.Add(            slist_range.Cells[i+2, 1].Value);
@@ -73,9 +73,6 @@ namespace WU_Einteilung
                 if (klist_range.Cells[i + 2, 4].Value == 1) kurse_klasse8.Add(true); else kurse_klasse8.Add(false);
                 if (klist_range.Cells[i + 2, 5].Value == 1) kurse_klasse9.Add(true); else kurse_klasse9.Add(false);
             }
-            //a1.Value = "1";
-            //string cellValue=a1.Value;
-            //MessageBox.Show(cellValue);
             wb.Save();
             wb.Close();
         }
@@ -88,46 +85,172 @@ namespace WU_Einteilung
         private void algorithmus()
         {
             List<int> zuloeschende_items = new List<int>();
-            int[] erstwahlen = new int[kurse_id.Count];
-            for (int n=0;n<erstwahlen.Length;n++)
+            List<int> kurs = new List<int>();
+            int[] wahlen = new int[kurse_id.Count];
+            var random = new Random();
+            #region Erstwahlen werden zugeordnet
+            for (int n=0;n<wahlen.Length;n++)
             {
-                erstwahlen[n] = 0;
+                wahlen[n] = 0; //Zum sichergehen, dass jedes Item von wahlen 0 ist
             }
-            for (int slist_counter=0; !String.Equals(slist_range.Cells[slist_counter+2, 2].Value,""); slist_counter++)
+            for (int slist_counter=0; slist_counter<schueler_id.Count; slist_counter++)
             {
                 for (int kid_counter=0; kid_counter < kurse_id.Count; kid_counter++)
                 {
                     if (String.Equals(kurse_id[kid_counter], slist_range.Cells[slist_counter + 2, 6].Value))
                     {
-                        erstwahlen[kid_counter]++;
+                        wahlen[kid_counter]++; //die Anzahl an Erstwählern wird in wahlen geschrieben
                     }
                 }
             }
             for (int kid_counter = 0; kid_counter < kurse_id.Count; kid_counter++)
             {
-                if (erstwahlen[kid_counter] <= kurse_maxpersonen[kid_counter])
+                if (wahlen[kid_counter] <= kurse_maxpersonen[kid_counter]) //Wenn es weniger oder gleich viele Erstwähler gibt, wie die maximale Größe des Kurses
                 {
                     for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
                     {
-                        slist_range.Cells[schueler_id[slist_counter] + 2, 9].Value = kurse_id[kid_counter];
-                        zuloeschende_items.Add(slist_counter);
+                        if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 6].Value))
+                        {
+                            slist_range.Cells[schueler_id[slist_counter] + 2, 9].Value = kurse_id[kid_counter]; //Jedem Schüler der diesen Kurs erstgewählt hat bekommt diesen zugeordnet
+                            kurse_maxpersonen[kid_counter]--; //Für jeden zugeordneten Schüler wird die maximale Größe verringert, die Variable zählt jetzt die freien Plätze
+                            zuloeschende_items.Add(slist_counter); //Jeder zugeordnete Schüler wird in diese Liste geschrieben um ihn später aus den schueler listen zu löschen
+                            //Es wird die Position des Schülers in den schueler Listen gespeichert
+                        }
+                    }
+                }
+                else if (wahlen[kid_counter] > kurse_maxpersonen[kid_counter]) //Wenn es mehr Erstwähler gibt, als der Kurs fassen kann sollen überschüssige Schüler zufällig ausgewählt werden
+                {
+                    kurs.Clear();// die Liste kurs wird gelert
+                    for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+                    {
+                        if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 6].Value))
+                        {
+                            kurs.Add(slist_counter);//die position der Schüler in den schueler listen wird in kurs geschrieben
+                        }
+                    }
+                    for (int n = 0; n <= wahlen[kid_counter] - kurse_maxpersonen[kid_counter]; n++)
+                    {
+                        kurs.Remove(kurs[random.Next(kurs.Count)]);//es werden überschüssige Schüler zufällig aus kurs gelöscht
+                    }
+                    for (int kurs_counter = 0; kurs_counter < kurs.Count; kurs_counter++)
+                    {
+                        slist_range.Cells[schueler_id[kurs[kurs_counter]] + 2, 9].Value = kurse_id[kid_counter]; //Allen Schülern, die noch in kurs drin sind wird der entsprechende Kurs zugeordnet
+                        kurse_maxpersonen[kid_counter]--; //Freie Plätze werden runtergezählt
+                        zuloeschende_items.Add(kurs[kurs_counter]);//
                     }
                 }
             }
+            zuloeschende_items.Sort();
             schueler_lists_reinigen(zuloeschende_items);
-            //for (int n = 0; n < zuloeschende_items.Count; n++)
-            //{
-            //    schueler_id.Remove(schueler_id[zuloeschende_items[n] - n]);
-            //    schueler_namen.Remove(schueler_namen[zuloeschende_items[n] - n]);
-            //    schueler_vornamen.Remove(schueler_vornamen[zuloeschende_items[n] - n]);
-            //    schueler_klasse.Remove(schueler_klasse[zuloeschende_items[n] - n]);
-            //    schueler_klassenlehrer.Remove(schueler_klassenlehrer[zuloeschende_items[n] - n]);
-            //    schueler_erstwahl.Remove(schueler_erstwahl[zuloeschende_items[n] - n]);
-            //    schueler_zweitwahl.Remove(schueler_zweitwahl[zuloeschende_items[n] - n]);
-            //    schueler_drittwahl.Remove(schueler_drittwahl[zuloeschende_items[n] - n]);
-            //}
-            zuloeschende_items = new List<int>();
+            #endregion
 
+            #region Zweitwahlen werden zugeordnet
+            zuloeschende_items.Clear();
+            for (int n = 0; n < wahlen.Length; n++)
+            {
+                wahlen[n] = 0;
+            }
+            for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+            {
+                for (int kid_counter = 0; kid_counter < kurse_id.Count; kid_counter++)
+                {
+                    if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 7].Value))
+                    {
+                        wahlen[kid_counter]++;
+                    }
+                }
+            }
+            for (int kid_counter = 0; kid_counter < kurse_id.Count; kid_counter++)
+            {
+                if (wahlen[kid_counter] <= kurse_maxpersonen[kid_counter])
+                {
+                    for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+                    {
+                        if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 7].Value))
+                        {
+                            slist_range.Cells[schueler_id[slist_counter] + 2, 9].Value = kurse_id[kid_counter];
+                            kurse_maxpersonen[kid_counter]--;
+                            zuloeschende_items.Add(slist_counter);
+                        }
+                    }
+                }
+                else if (wahlen[kid_counter] > kurse_maxpersonen[kid_counter])
+                {
+                    kurs.Clear();
+                    for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+                    {
+                        if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 7].Value))
+                        {
+                            kurs.Add(slist_counter);
+                        }
+                    }
+                    for (int n = 0; n <= wahlen[kid_counter] - kurse_maxpersonen[kid_counter]; n++)
+                    {
+                        kurs.Remove(kurs[random.Next(kurs.Count)]);
+                    }
+                    for (int kurs_counter = 0; kurs_counter < kurs.Count; kurs_counter++)
+                    {
+                        slist_range.Cells[schueler_id[kurs[kurs_counter]] + 2, 9].Value = kurse_id[kid_counter];
+                        kurse_maxpersonen[kid_counter]--;
+                        zuloeschende_items.Add(kurs[kurs_counter]);
+                    }
+                }
+            }
+            #endregion
+
+            #region Drittwahlen werden zugeordnet
+            zuloeschende_items.Clear();
+            for (int n = 0; n < wahlen.Length; n++)
+            {
+                wahlen[n] = 0;
+            }
+            for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+            {
+                for (int kid_counter = 0; kid_counter < kurse_id.Count; kid_counter++)
+                {
+                    if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 7].Value))
+                    {
+                        wahlen[kid_counter]++;
+                    }
+                }
+            }
+            for (int kid_counter = 0; kid_counter < kurse_id.Count; kid_counter++)
+            {
+                if (wahlen[kid_counter] <= kurse_maxpersonen[kid_counter])
+                {
+                    for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+                    {
+                        if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 7].Value))
+                        {
+                            slist_range.Cells[schueler_id[slist_counter] + 2, 9].Value = kurse_id[kid_counter];
+                            kurse_maxpersonen[kid_counter]--;
+                            zuloeschende_items.Add(slist_counter);
+                        }
+                    }
+                }
+                else if (wahlen[kid_counter] > kurse_maxpersonen[kid_counter])
+                {
+                    kurs.Clear();
+                    for (int slist_counter = 0; slist_counter < schueler_id.Count; slist_counter++)
+                    {
+                        if (String.Equals(kurse_id[kid_counter], slist_range.Cells[schueler_id[slist_counter] + 2, 7].Value))
+                        {
+                            kurs.Add(slist_counter);
+                        }
+                    }
+                    for (int n = 0; n <= wahlen[kid_counter] - kurse_maxpersonen[kid_counter]; n++)
+                    {
+                        kurs.Remove(kurs[random.Next(kurs.Count)]);
+                    }
+                    for (int kurs_counter = 0; kurs_counter < kurs.Count; kurs_counter++)
+                    {
+                        slist_range.Cells[schueler_id[kurs[kurs_counter]] + 2, 9].Value = kurse_id[kid_counter];
+                        kurse_maxpersonen[kid_counter]--;
+                        zuloeschende_items.Add(kurs[kurs_counter]);
+                    }
+                }
+            }
+            #endregion
 
         }
 
